dd00b11d0a0472265ba59ae475047c91185f67af
thaisfreires <thaisfrsoares@gmail.com>
2024-12-05 17:58:23 +0000

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       OpenSans: ['OpenSans', 'sans-serif'],
+      }, 
+    },
   },
   plugins: [],
-}
+};
 
